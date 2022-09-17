b7b43ffec4af34d3551b3e86c870143ea83fcd7a
de4n_whit3 <de4nwhit3@gmail.com>
2022-09-17 09:58:29 +0200

    Completed groups exercise

---
+++ chapter-6/group.js
@@ -0,0 +1,35 @@
+class Group{
+    constructor(){
+        this.group = [];
+    }
+
+    add(element){
+        if(!this.has(element)){
+            this.group.push(element);
+        }
+    }
+
+    delete(itemToDelete){
+        this.group = this.group.filter(element => element != itemToDelete)
+    }
+
+    has(element){
+        return this.group.includes(element);
+    }
+
+    static from(elements){
+        let group = new Group();
+        for(let element of elements){
+            group.add(element);
+        }
+        return group;
+    }
+}
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+console.log(group.has(30));
+group.add(10);
+console.log(group.has(10));
+group.delete(10);
+console.log(group.has(10));
