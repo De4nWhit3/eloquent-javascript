20f985584924c2dc93864e6f87ea18f50aff9ef3
de4n_whit3 <de4nwhit3@gmail.com>
2022-09-19 20:31:10 +0200

    more boilerplate

--- chapter-7/efficientbot.js
+++ chapter-7/efficientbot.js
@@ -23,14 +23,62 @@ function buildGraph(edges){
 
 // array of places, and destinations that can be reached from there
 export const roadGraph = buildGraph(roads);
-let first = new VillageState(
-    "Post Office", 
-    [{place: "Post Office", address: "Alice's House"}]
-);
 
-let next = first.move("Alice's House");
+// let first = new VillageState(
+//     "Post Office", 
+//     [{place: "Post Office", address: "Alice's House"}]
+// );
 
-console.log("next.place: ", next.place);
-console.log("next.parcels: ", next.parcels);
-console.log("first.place: ", first.place);
-console.log("first.parcels: ", first.parcels);
+// let next = first.move("Alice's House");
+
+// console.log("next.place: ", next.place);
+// console.log("next.parcels: ", next.parcels);
+// console.log("first.place: ", first.place);
+// console.log("first.parcels: ", first.parcels);
+
+function runRobot(state, robot, memory){
+    for(let turn = 0;; turn++){
+        if(state.parcels.length == 0){
+            console.log(`Done in ${turn} turns`);
+            break;
+        }
+
+        let action = robot(state, memory);
+        state = state.move(action.direction);
+        memory = action.memory;
+        console.log(`Moved to ${action.direction}`);
+    }
+}
+
+// random bot code
+function randomPick(array){
+    let choice = Math.floor(Math.random() * array.length);
+    return array[choice];
+}
+
+function randomRobot(state){
+    return {
+        direction: randomPick(roadGraph[state.place])
+    };
+}
+
+// create a new state with some parcels
+VillageState.random = function(parcelCount = 5){
+    let parcels = [];
+
+    for(let i = 0; i < parcelCount; i++){
+        let address = randomPick(Object.keys(roadGraph));
+        let place;
+
+        do{
+            place = randomPick(Object.keys(roadGraph));
+        }while(place == address);
+
+        parcels.push({ place, address });
+    }
+
+    // TODO: Set a mail route
+    return new VillageState("Post Office", parcels);
+}
+
+runRobot(VillageState.random(), randomRobot);
