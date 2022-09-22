9874c4237cdbdcc079f123b8634b8e9d240cb689
de4n_whit3 <de4nwhit3@gmail.com>
2022-09-22 23:55:26 +0200

    completed box exercise

---
+++ chapter-8/lockedbox.js
@@ -0,0 +1,45 @@
+const box = {
+    locked: true,
+    unlock(){
+        this.locked = false;
+    },
+    lock(){
+        this.locked = true;
+    },
+    _content: [],
+    get content(){
+        if(this.locked){
+            throw new Error("Locked!");
+        }
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body){
+    try{
+        if(!box.locked){
+            return body();
+        }
+
+        box.unlock();
+        return body();
+    }catch(e){
+        //
+    }finally{
+        box.lock();
+    }
+}
+
+withBoxUnlocked(function(){
+    box.content.push("gold piece");
+});
+
+try{
+    withBoxUnlocked(function(){
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+}catch(e){
+    console.log("Error raised: " + e);
+}
+
+console.log(box.locked);
